fa3702a6db683a3199aab40e1214335e88b1d6f1
Brajesh97 <imbrajesh97@gmail.com>
2022-02-28 19:19:27 +0530

    frstt commimt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Form1 from "./Form1";
-//import Form, {Form} from "./Form1";
+//import Form, {Form2} from "./Form1";
 const App = () => {
         return(
             <div>
